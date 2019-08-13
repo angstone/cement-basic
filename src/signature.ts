53595816a9b53583bff08744f4e71269f90fb5ba
ssribeiro <luan@ssribeiro.com>
2019-08-13 08:18:10 -0300

    changed the signature to include all environment variables.

--- src/signature.ts
+++ src/signature.ts
@@ -3,11 +3,15 @@ import { env, logger } from './'
 export const signature = () => {
   logger.dash()
   logger.note(`
-   APP: ${env.APP_NAME}
-   Description: ${env.APP_DESCRIPTION}
-   version: ${env.APP_VERSION}
-   Author: ${env.APP_AUTHOR}
-   License: ${env.APP_LICENSE}
-   loaded at ${env.APP_LOADED_AT}
+   APP_NAME: ${env.APP_NAME}
+   APP_DESCRIPTION: ${env.APP_DESCRIPTION}
+   APP_VERSION: ${env.APP_VERSION}
+   APP_AUTHOR: ${env.APP_AUTHOR}
+   APP_LICENSE: ${env.APP_LICENSE}
+
+   APP_ENV: ${env.APP_ENV}
+   APP_LOGLEVEL: ${env.APP_LOGLEVEL}
+
+   APP_LOADED_AT: ${env.APP_LOADED_AT}
   `)
 }
