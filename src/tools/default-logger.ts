2aa4a6cc1eec3c4d114b0b54eabc12f2ab16f615
ssribeiro <luan@ssribeiro.com>
2019-08-23 15:39:57 -0300

    automatic stringify non-string logs. Dev logs are green when string

--- src/tools/default-logger.ts
+++ src/tools/default-logger.ts
@@ -5,27 +5,59 @@ export const DASH_LINE = `______________________________________________________
 
 /* tslint:disable:no-console */
 const fatal = (...args: any[]) => {
-  console.log(chalk.red(...args))
+  const chalkedArgs: any[] = []
+  args.forEach((arg: any) => {
+    if(typeof arg === 'string') {
+      chalkedArgs.push(chalk.red(arg))
+    } else {
+      chalkedArgs.push(chalk.red(JSON.stringify(arg)))
+    }
+  })
+  console.log(...chalkedArgs)
 }
 
 const error = (...args: any[]) => {
   const currentLoglevel: number = LOGLEVEL_NUMERIC[env.APP_LOGLEVEL!]
   if (currentLoglevel >= LOGLEVEL_NUMERIC[LOGLEVELS.ERROR]) {
-    console.log(chalk.yellow(...args))
+    const chalkedArgs: any[] = []
+    args.forEach((arg: any) => {
+      if(typeof arg === 'string') {
+        chalkedArgs.push(chalk.yellow(arg))
+      } else {
+        chalkedArgs.push(chalk.yellow(JSON.stringify(arg)))
+      }
+    })
+    console.log(...chalkedArgs)
   }
 }
 
 const note = (...args: any[]) => {
   const currentLoglevel: number = LOGLEVEL_NUMERIC[env.APP_LOGLEVEL!]
   if (currentLoglevel >= LOGLEVEL_NUMERIC[LOGLEVELS.NOTE]) {
-    console.log(chalk.blue(...args))
+    const chalkedArgs: any[] = []
+    args.forEach((arg: any) => {
+      if(typeof arg === 'string') {
+        chalkedArgs.push(chalk.blue(arg))
+      } else {
+        chalkedArgs.push(chalk.blue(JSON.stringify(arg)))
+      }
+    })
+    console.log(...chalkedArgs)
   }
 }
 
 const dev = (...args: any[]) => {
   const currentLoglevel: number = LOGLEVEL_NUMERIC[env.APP_LOGLEVEL!]
   if (currentLoglevel >= LOGLEVEL_NUMERIC[LOGLEVELS.DEV_NOTE]) {
-    console.log(...args)
+    const chalkedArgs: any[] = []
+    args.forEach((arg: any) => {
+      if(typeof arg === 'string') {
+        chalkedArgs.push(chalk.green(arg))
+      } else {
+        chalkedArgs.push(arg)
+      }
+    })
+    console.log(...chalkedArgs)
   }
 }
 
